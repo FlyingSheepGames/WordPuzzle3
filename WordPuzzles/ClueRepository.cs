0b3c60478d1fb6ac77e906d9f013ce8fa2eec6ff
FlyingSheepGames <DungeonHero@FlyingSheep.com>
2020-05-25 08:05:23 -0400

    Create Clue Repository
    Support adding clues, looking up clues, and file persistence. All test pass.

---
+++ WordPuzzles/ClueRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+
+namespace WordPuzzles
+{
+    public class ClueRepository
+    {
+        public int ClueCount => Clues.Count;
+
+        private Dictionary<string, List<string>> Clues = new Dictionary<string, List<string>>();
+
+        public void AddClue(string word, string clue)
+        {
+            string canonicalWord = word.ToUpperInvariant();
+            List<string> currentCluesForWord = null;
+            if (Clues.ContainsKey(canonicalWord))
+            {
+                currentCluesForWord = Clues[canonicalWord];
+            }
+
+            if (currentCluesForWord == null)
+            {
+                currentCluesForWord = new List<string>();
+            }
+
+            if (!currentCluesForWord.Contains(clue))
+            {
+                currentCluesForWord.Add(clue);
+            }
+
+            Clues[canonicalWord] = currentCluesForWord;
+        }
+
+        public List<string> GetCluesForWord(string word)
+        {
+            string canonicalWord = word.ToUpperInvariant();
+            if (Clues.ContainsKey(canonicalWord))
+            {
+                return Clues[canonicalWord];
+            }
+            return new List<string>();
+        }
+
+        public void WriteToDisk(string fileLocation)
+        {
+            File.WriteAllText(fileLocation, JsonConvert.SerializeObject(Clues));
+        }
+
+        public void ReadFromDisk(string fileLocation)
+        {
+            Clues = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText(fileLocation));
+        }
+    }
+}
---
+++ WordPuzzlesTest.NetFramework/ClueRepositoryTest.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using WordPuzzles;
+
+namespace WordPuzzlesTest
+{
+    [TestFixture]
+    public class ClueRepositoryTest
+    {
+        [TestFixture]
+        public class AddClue
+        {
+            [Test]
+            public void IncrementsClueCount()
+            {
+                ClueRepository repository = new ClueRepository();
+                Assert.AreEqual(0, repository.ClueCount, "Repository should start out empty");
+                repository.AddClue("ONES", "Singletons");
+
+                Assert.AreEqual(1, repository.ClueCount, "Repository should have one clue now.");
+            }
+
+            [Test]
+            public void SupportsMultipleClues()
+            {
+                ClueRepository repository = new ClueRepository();
+                Assert.AreEqual(0, repository.ClueCount, "Repository should start out empty");
+                repository.AddClue("ONES", "Singletons");
+                repository.AddClue("ONES", "Smallest denomination of folding money.");
+
+                Assert.AreEqual(1, repository.ClueCount, "Repository should have one clue now.");
+            }
+
+        }
+
+        [TestFixture]
+        public class GetCluesForWord
+        {
+            [Test]
+            public void NoClues_ReturnsEmptyList()
+            {
+                ClueRepository repository = new ClueRepository();
+                List<string> clues=  repository.GetCluesForWord("Clueless"); 
+                Assert.AreEqual(0, clues.Count);
+            }
+
+            [Test]
+            public void SupportsMultipleClues()
+            {
+                ClueRepository repository = new ClueRepository();
+                Assert.AreEqual(0, repository.ClueCount, "Repository should start out empty");
+                repository.AddClue("ONES", "Singletons");
+                repository.AddClue("ONES", "Smallest denomination of folding money.");
+                List<string> clues = repository.GetCluesForWord("ONES");
+                Assert.AreEqual(2, clues.Count);
+            }
+
+            [Test]
+            public void IsCaseInsensitive()
+            {
+                ClueRepository repository = new ClueRepository();
+                Assert.AreEqual(0, repository.ClueCount, "Repository should start out empty");
+                repository.AddClue("ones", "Singletons");
+                repository.AddClue("OnEs", "Smallest denomination of folding money.");
+                List<string> clues = repository.GetCluesForWord("Ones");
+                Assert.AreEqual(2, clues.Count);
+            }
+
+        }
+
+        [TestFixture]
+        public class WriteToDisk
+        {
+            [Test]
+            public void EmptyRepository_CreatesExpectedFile()
+            {
+                ClueRepository repository = new ClueRepository();
+                repository.WriteToDisk(@"data\actual_empty_clue_repository.json");
+
+                var expectedLines= File.ReadAllLines(@"data\expected_empty_clue_repository.json");
+                var actualLines = File.ReadAllLines(@"data\actual_empty_clue_repository.json");
+                for (var index = 0; index < expectedLines.Length; index++)
+                {
+                    string expectedLine = expectedLines[index];
+                    string actualLine = actualLines[index];
+                    Assert.AreEqual(expectedLine, actualLine, $"Unexpected line at index {index}");
+                }
+            }
+            [Test]
+            public void MultipleClues_CreatesExpectedFile()
+            {
+                ClueRepository repository = new ClueRepository();
+                repository.AddClue("ONES", "Singletons");
+                repository.AddClue("ONES", "Smallest denomination of folding money.");
+
+                repository.AddClue("Lego", "A small colorful brick.");
+
+                repository.WriteToDisk(@"data\actual_nonempty_clue_repository.json");
+
+                var expectedLines = File.ReadAllLines(@"data\expected_nonempty_clue_repository.json");
+                var actualLines = File.ReadAllLines(@"data\actual_nonempty_clue_repository.json");
+                for (var index = 0; index < expectedLines.Length; index++)
+                {
+                    string expectedLine = expectedLines[index];
+                    string actualLine = actualLines[index];
+                    Assert.AreEqual(expectedLine, actualLine, $"Unexpected line at index {index}");
+                }
+            }
+        }
+
+        [TestFixture]
+        public class ReadFromDisk
+        {
+            [Test]
+            public void MultipleClues_PopulatesClueDictionry()
+            {
+                ClueRepository repository = new ClueRepository();
+
+                repository.ReadFromDisk(@"data\expected_nonempty_clue_repository.json");
+
+                Assert.AreEqual(2, repository.ClueCount);
+                List<string> clues = repository.GetCluesForWord("ONES");
+                Assert.AreEqual(2, clues.Count);
+                List<string> cluesForLego = repository.GetCluesForWord("Lego");
+                Assert.AreEqual(1, cluesForLego.Count);
+
+            }
+        }
+    }
+}
