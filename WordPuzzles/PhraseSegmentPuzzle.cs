f477453e7a565889a5823bf8a39f77077f1852f2
FlyingSheepGames <DungeonHero@FlyingSheep.com>
2020-07-10 13:04:32 -0400

    New Puzzle Type - Phrase Segment Puzzle
    Create underlying data. Next step is UI.  All tests pass.

---
+++ WordPuzzles/PhraseSegmentPuzzle.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordPuzzles
+{
+    public class PhraseSegmentPuzzle
+    {
+        public string Phrase { get; set; }
+        public string Author { get; set; }
+        public int CompleteLength { get; set; }
+
+        public void PlacePhrase()
+        {
+            CompleteLength = Phrase.Length + Author.Length + 1;
+
+            int distanceToNextMultipleOfFour = (CompleteLength % 4);
+            if (distanceToNextMultipleOfFour == 0)
+            {
+                SpacesBeforeAuthor = 1;
+            }
+            else
+            {
+                SpacesBeforeAuthor = 5 - distanceToNextMultipleOfFour;
+            }
+            CompleteLength = Phrase.Length + Author.Length + SpacesBeforeAuthor;
+            LineLength = CompleteLength / 4;
+            foreach (int blockSize in CalculateBlockSizes(LineLength))
+            {
+                Blocks.Add(new Block()
+                {
+                    Width = blockSize
+                });
+
+            }
+        }
+
+        public int LineLength { get; set; }
+
+        internal List<int> CalculateBlockSizes(int lineLength)
+        {
+            var blockSizes = new List<int>();
+            int numberOfBlocks = lineLength / 4;
+            for (int i = 0; i < numberOfBlocks; i++)
+            {
+                blockSizes.Add(4);
+            }
+            switch (lineLength% 4)
+            {
+                case 0:
+                    break;
+                case 1: //make second one size 5
+                    blockSizes[1] = 5;
+                    break;
+                case 2: //make second one size 3, and add a 3 at the end.
+                    blockSizes[1] = 3;
+                    blockSizes.Add(3);
+                    break;
+                case 3:
+                    blockSizes.Add(3);
+                    break;
+            }
+            return blockSizes;
+        }
+
+        public List<Block> Blocks = new List<Block>();
+
+        public int SpacesBeforeAuthor { get; set; }
+
+        public string CompletePhrase
+        {
+            get
+            {
+                return Phrase + new string(' ', SpacesBeforeAuthor) + Author;
+            }
+        }
+
+        public List<Block> BreakPhraseIntoBlocks(string completePhrase, List<int> blockWidths)
+        {
+            var blocksToReturn = new List<Block>();
+            int lineLengthSoFar = 0;
+            foreach (int blockWidth in blockWidths)
+            {
+                blocksToReturn.Add(CreateBlock(completePhrase, lineLengthSoFar, blockWidth));
+                lineLengthSoFar += blockWidth;
+            }
+            return blocksToReturn;
+        }
+
+        private Block CreateBlock(string completePhrase, int lineLengthSoFar, int nextWidthToTake)
+        {
+            var blockToReturn = new Block();
+            blockToReturn.Width = nextWidthToTake;
+            blockToReturn.Lines = new List<string>();
+            int singleLineLength = completePhrase.Length / 4;
+            blockToReturn.Fragments = new List<string>();
+            for (int lineIndex = 0; lineIndex < 4; lineIndex++)
+            {
+                var subString = GetSubString(completePhrase, lineLengthSoFar, nextWidthToTake, lineIndex, singleLineLength);
+                blockToReturn.Lines.Add(subString);
+                blockToReturn.Fragments.AddRange(subString.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries));
+            }
+            return blockToReturn;
+        }
+
+        internal string GetSubString(string completePhrase, int lineLengthSoFar, int nextWidthToTake, int lineIndex,
+            int singleLineLength)
+        {
+            int startIndex = lineLengthSoFar;
+            startIndex += lineIndex * singleLineLength;
+            return completePhrase.Substring(startIndex, nextWidthToTake);
+        }
+
+    }
+
+    public class Block
+    {
+        public int Width { get; set; }
+        public List<string> Lines { get; set; }
+        public List<string> Fragments { get; set; }
+    }
+}
---
+++ WordPuzzlesTest.NetFramework/PhraseSegmentPuzzleTest.cs
@@ -0,0 +1,172 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using WordPuzzles;
+
+namespace WordPuzzlesTest
+{
+    [TestFixture]
+    public class PhraseSegmentPuzzleTest
+    {
+        [TestFixture]
+        public class PlacePhrase
+        {
+            [Test]
+            public void ExampleOne_SetsExpectedProperties()
+            {
+                const string AUTHOR = "Chip";
+                const string PHRASE = "The fat cat sat on the bat.";
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle
+                {
+                    Phrase = PHRASE, 
+                    Author = AUTHOR
+                };
+
+                puzzle.PlacePhrase();
+                Assert.AreEqual(32, puzzle.CompleteLength, "Unexpected Complete Length");
+                Assert.AreEqual(1, puzzle.SpacesBeforeAuthor, "Unexpected SpacesBeforeAuthor");
+                Assert.AreEqual(PHRASE + " " + AUTHOR, puzzle.CompletePhrase, "Unexpected CompletePhrase");
+
+            }
+
+            [Test]
+            public void ExampleTwo_SetsExpectedProperties()
+            {
+                const string AUTHOR = "Chip";
+                const string PHRASE = "The fat cat sat in the bath.";
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle
+                {
+                    Phrase = PHRASE,
+                    Author = AUTHOR
+                };
+
+                puzzle.PlacePhrase();
+                Assert.AreEqual(36, puzzle.CompleteLength, "Unexpected Complete Length");
+                Assert.AreEqual(4, puzzle.SpacesBeforeAuthor, "Unexpected SpacesBeforeAuthor");
+                Assert.AreEqual(PHRASE + "    " + AUTHOR, puzzle.CompletePhrase, "Unexpected CompletePhrase");
+
+            }
+
+        }
+
+        [TestFixture]
+        public class CalculateBlockSizes
+        {
+            [Test]
+            public void ExampleZeroModFour_ReturnsExpectedSizes()
+            {
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle();
+                List<int> sizes = puzzle.CalculateBlockSizes(8);
+                Assert.AreEqual(2, sizes.Count, "Expected 2 integers");
+
+                Assert.AreEqual(4, sizes[0], "Unexpected first integer");
+                Assert.AreEqual(4, sizes[1], "Unexpected second integer");
+            }
+
+            [Test]
+            public void ExampleOneModFour_ReturnsExpectedSizes()
+            {
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle();
+                List<int> sizes = puzzle.CalculateBlockSizes(9);
+                Assert.AreEqual(2, sizes.Count, "Expected 2 integers");
+
+                Assert.AreEqual(4, sizes[0], "Unexpected first integer");
+                Assert.AreEqual(5, sizes[1], "Unexpected second integer");
+            }
+
+            [Test]
+            public void ExampleTwoModFour_ReturnsExpectedSizes()
+            {
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle();
+                List<int> sizes = puzzle.CalculateBlockSizes(10);
+                Assert.AreEqual(3, sizes.Count, "Expected 3 integers");
+
+                Assert.AreEqual(4, sizes[0], "Unexpected first integer");
+                Assert.AreEqual(3, sizes[1], "Unexpected second integer");
+                Assert.AreEqual(3, sizes[2], "Unexpected third integer");
+            }
+
+            [Test]
+            public void ExampleThreeModFour_ReturnsExpectedSizes()
+            {
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle();
+                List<int> sizes = puzzle.CalculateBlockSizes(11);
+                Assert.AreEqual(3, sizes.Count, "Expected 3 integers");
+
+                Assert.AreEqual(4, sizes[0], "Unexpected first integer");
+                Assert.AreEqual(4, sizes[1], "Unexpected second integer");
+                Assert.AreEqual(3, sizes[2], "Unexpected third integer");
+            }
+
+            [Test]
+            public void ExampleOneModFourButBigger_ReturnsExpectedSizes()
+            {
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle();
+                List<int> sizes = puzzle.CalculateBlockSizes(13);
+                Assert.AreEqual(3, sizes.Count, "Expected 3 integers");
+
+                Assert.AreEqual(4, sizes[0], "Unexpected first integer");
+                Assert.AreEqual(5, sizes[1], "Unexpected second integer");
+                Assert.AreEqual(4, sizes[2], "Unexpected third integer");
+            }
+
+            [Test]
+            public void GreaterThanFourteen_EqualsSumOfSizes()
+            {
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle();
+                for (int lineLength = 14; lineLength < 101; lineLength++)
+                {
+                    int sum = 0;
+                    foreach (int blockSize in puzzle.CalculateBlockSizes(lineLength))
+                    {
+                        sum += blockSize;
+                    }
+                    Assert.AreEqual(lineLength, sum, "Expected sum of block sizes to equal line length.");
+                }
+            }
+        }
+
+        [TestFixture]
+        public class BreakPhraseIntoBlocks
+        {
+
+            [Test]
+            public void ExampleOne_CreatesExpectedBlocks()
+            {
+                const string AUTHOR = "Chip";
+                const string PHRASE = "The fat cat sat on the bat.";
+
+                PhraseSegmentPuzzle puzzle = new PhraseSegmentPuzzle();
+
+                List<Block> blocks = puzzle.BreakPhraseIntoBlocks(PHRASE + " " + AUTHOR, new List<int> {4, 4});
+
+                Assert.AreEqual(2, blocks.Count, "Expected two blocks.");
+                var firstBlock = blocks[0];
+
+                Assert.AreEqual(4, firstBlock.Width, "Unexpected Width for firstBlock");
+                Assert.AreEqual(4, firstBlock.Lines.Count, "Unexpected Number of Lines for FirstBlock");
+
+                Assert.AreEqual("The ", firstBlock.Lines[0], "Unexpected first line in firstBlock");
+                Assert.AreEqual("cat ", firstBlock.Lines[1], "Unexpected second line in firstBlock");
+                Assert.AreEqual("on t", firstBlock.Lines[2], "Unexpected third line in firstBlock");
+                Assert.AreEqual("at. ", firstBlock.Lines[3], "Unexpected fourth line in firstBlock");
+
+                CollectionAssert.AreEquivalent(new[] {"The", "cat", "on", "t", "at."}, 
+                    firstBlock.Fragments, "Unexpected set of Fragments");
+
+                var secondBlock = blocks[1];
+
+                Assert.AreEqual(4, secondBlock.Width, "Unexpected Width for secondBlock");
+                Assert.AreEqual(4, secondBlock.Lines.Count, "Unexpected Number of Lines for secondBlock");
+
+                Assert.AreEqual("fat ", secondBlock.Lines[0], "Unexpected first line in secondBlock");
+                Assert.AreEqual("sat ", secondBlock.Lines[1], "Unexpected second line in secondBlock");
+                Assert.AreEqual("he b", secondBlock.Lines[2], "Unexpected third line in secondBlock");
+                Assert.AreEqual("Chip", secondBlock.Lines[3], "Unexpected fourth line in secondBlock");
+
+                CollectionAssert.AreEquivalent(new[] { "fat", "sat", "he", "b", "Chip" },
+                    secondBlock.Fragments, "Unexpected set of Fragments");
+
+            }
+        }
+    }
+}
