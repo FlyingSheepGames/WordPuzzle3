f73bfda22dfcbb99290014a5a7ff9a35e14c8583
FlyingSheepGames <DungeonHero@FlyingSheep.com>
2020-07-17 19:10:22 -0400

    New Puzzle Type Hidden Related Words Puzzle
    Preliminary calculations for displaying the word. All tests pass.

---
+++ WordPuzzles/HiddenRelatedWordsPuzzle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace WordPuzzles
+{
+    public class HiddenRelatedWordsPuzzle
+    {
+        List<HiddenWord> hiddenWords = new List<HiddenWord>();
+
+        public int CombinedKeyIndex
+        {
+            get
+            {
+                int maxKeyIndexSoFar = -1;
+                foreach (var word in hiddenWords)
+                {
+                    if (maxKeyIndexSoFar < word.KeyIndex)
+                    {
+                        maxKeyIndexSoFar = word.KeyIndex;
+                    }
+                }
+
+                return maxKeyIndexSoFar;
+            }
+        }
+
+        public int CombinedLength
+        {
+            get
+            {
+                int maxLettersAfterIndex = -1;
+                foreach (var word in hiddenWords)
+                {
+                    if (maxLettersAfterIndex < word.LettersAfterIndex)
+                    {
+                        maxLettersAfterIndex = word.LettersAfterIndex;
+                    }
+                }
+
+                return maxLettersAfterIndex + CombinedKeyIndex + 1;
+            }
+        }
+
+        public void AddWord(HiddenWord hiddenWordToAdd)
+        {
+            hiddenWords.Add(hiddenWordToAdd);
+        }
+    }
+
+    public class HiddenWord
+    {
+        public string Word { get; set; }
+        public int KeyIndex { get; set; }
+
+        public int LettersAfterIndex
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Word)) return -1;
+                return Word.Length - (KeyIndex + 1);
+            }
+        }
+
+        public string SentenceHidingWord { get; set; }
+    }
+}
---
+++ WordPuzzlesTest.NetFramework/HiddenRelatedWordsPuzzleTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using NUnit.Framework;
+using WordPuzzles;
+
+namespace WordPuzzlesTest
+{
+    [TestFixture]
+    public class HiddenRelatedWordsPuzzleTest
+    {
+        [TestFixture]
+        public class AddWord
+        {
+            [Test]
+            public void CalculatesCombinedIndexAndLength()
+            {
+
+                HiddenRelatedWordsPuzzle puzzle = new HiddenRelatedWordsPuzzle();
+                puzzle.AddWord(new HiddenWord()
+                {
+                    Word = "holder",
+                    KeyIndex = 3,
+                    SentenceHidingWord = "You'll have to hold ermine and skunks in this job."
+                });
+
+                Assert.AreEqual(3, puzzle.CombinedKeyIndex, "Unexpected Combined Key Index");
+                Assert.AreEqual(6, puzzle.CombinedLength, "Unexpected Combined Length");
+
+                puzzle.AddWord(new HiddenWord()
+                {
+                    Word = "table",
+                    KeyIndex = 4,
+                    SentenceHidingWord = "He wasn't able to keep his foxes straight."
+                });
+                Assert.AreEqual(4, puzzle.CombinedKeyIndex, "Unexpected Combined Key Index");
+                Assert.AreEqual(7, puzzle.CombinedLength, "Unexpected Combined Length");
+
+                puzzle.AddWord(new HiddenWord()
+                {
+                    Word = "stand",
+                    KeyIndex = 0,
+                    SentenceHidingWord = "I lived somewhere between east and west."
+                });
+                Assert.AreEqual(4, puzzle.CombinedKeyIndex, "Unexpected Combined Key Index");
+                Assert.AreEqual(9, puzzle.CombinedLength, "Unexpected Combined Length");
+
+                puzzle.AddWord(new HiddenWord()
+                {
+                    Word = "rack",
+                    KeyIndex = 3,
+                    SentenceHidingWord = "You'd better acknowledge my superiority in this matter."
+                });
+                Assert.AreEqual(4, puzzle.CombinedKeyIndex, "Unexpected Combined Key Index");
+                Assert.AreEqual(9, puzzle.CombinedLength, "Unexpected Combined Length");
+            }
+        }
+    }
+
+    [TestFixture]
+    public class HiddenWordTest
+    {
+        [TestFixture]
+        public class Constructor
+        {
+            [Test]
+            public void CalculatesLettersAfterIndex()
+            {
+                var hiddenWord = new HiddenWord()
+                {
+                    Word = "holder",
+                    KeyIndex = 3,
+                };
+                Assert.AreEqual(2, hiddenWord.LettersAfterIndex);
+            }
+
+        }
+    }
+}
