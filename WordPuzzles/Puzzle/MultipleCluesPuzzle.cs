d2c0207c9d3f9a3c8dce206b7f82d3e13855b0f0
FlyingSheepGames <DungeonHero@FlyingSheep.com>
2020-10-04 16:41:07 -0400

    Add MultipleClues Puzzle
    Solve the puzzle by figuring out which clues go together. All tests pass.

---
+++ WordPuzzles/Puzzle/MultipleCluesPuzzle.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WordPuzzles.Utility;
+
+namespace WordPuzzles.Puzzle
+{
+    public class MultipleCluesPuzzle
+    {
+        private Random _randomNumberGenerator;
+        public List<WordWithClues> WordsWithClues = new List<WordWithClues>();
+        internal int NextClueOrder = 1;
+
+        public WordRepository Repository { get; set; } = new WordRepository();
+
+        public int RandomGeneratorSeed { get; set; } = 0;
+        public Random RandomNumberGenerator
+        {
+            get
+            {
+                if (_randomNumberGenerator == null)
+                {
+                    _randomNumberGenerator = new Random(RandomGeneratorSeed);
+                }
+                return _randomNumberGenerator;
+            }
+        }
+
+        public List<string> GetCandidatesForLetter(char secondLetter)
+        {
+            var candidatesForLetter = new List<string>();
+            StringBuilder builder = new StringBuilder();
+            for (int wordLength = 2; wordLength <= 7; wordLength++)
+            {
+                builder.Clear();
+                builder.Append('_');
+                builder.Append(secondLetter);
+                builder.Append('_', wordLength - 2);
+                candidatesForLetter.AddRange(
+                    Repository.WordsMatchingPattern(builder.ToString()));
+
+            }
+            return candidatesForLetter;
+        }
+
+        public void AddWordWithClues(string wordToAdd, List<string> clues)
+        {
+            WordWithClues wordWithClues = new WordWithClues()
+            {
+                WordText = wordToAdd,
+                Clues = new List<OrderedClue>(),
+            };
+            foreach (var clue in clues)
+            {
+                wordWithClues.Clues.Add(new OrderedClue()
+                {
+                    ClueText = clue, 
+                    ClueOrder = NextClueOrder++,
+                });   
+            }
+            WordsWithClues.Add(wordWithClues);
+        }
+
+        public void ReorderClues(int stackCounter = 0)
+        {
+            if (3 < stackCounter)
+            {
+                throw new Exception("Tried to reorder clues at least 3 times.");
+            }
+            //Swap the order of a clue from two different words. Do this 5 times. 
+            if (WordsWithClues.Count < 3) return;
+
+            for (int swapCount = 0; swapCount < 6; swapCount++)
+            {
+                int firstWordIndex = RandomNumberGenerator.Next(WordsWithClues.Count);
+                int secondWordIndex = RandomNumberGenerator.Next(WordsWithClues.Count);
+                if (firstWordIndex == secondWordIndex) continue;
+                var firstWord = WordsWithClues[firstWordIndex];
+                var secondWord = WordsWithClues[secondWordIndex];
+                firstWord.Clues.Shuffle(RandomNumberGenerator);
+                secondWord.Clues.Shuffle(RandomNumberGenerator);
+
+                int firstWordClueOrder = firstWord.Clues[0].ClueOrder;
+                firstWord.Clues[0].ClueOrder = secondWord.Clues[0].ClueOrder;
+                secondWord.Clues[0].ClueOrder = firstWordClueOrder;
+            }
+            //a few checks
+            List<int> uniqueSums = new List<int>();
+            bool allSumsUnique = true;
+            foreach (var word in WordsWithClues)
+            {
+                if (uniqueSums.Contains(word.SumOfClueOrders))
+                {
+                    allSumsUnique = false;
+                    break;
+                }
+                uniqueSums.Add(word.SumOfClueOrders);
+            }
+
+            if (!allSumsUnique)
+            {
+                ReorderClues(stackCounter+1);
+            }
+        }
+    }
+
+    public class OrderedClue
+    {
+        public string ClueText { get; set; }
+        public int ClueOrder { get; set; }
+    }
+
+    public class WordWithClues
+    {
+        public string WordText { get; set; }
+        public List<OrderedClue> Clues { get; set; }
+        public int SumOfClueOrders
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var clue in Clues)
+                {
+                    sum += clue.ClueOrder;
+                }
+
+                return sum;
+            }
+        }
+    }
+}
---
+++ WordPuzzlesTest/Puzzle/MultipleCluesPuzzleTest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using WordPuzzles.Puzzle;
+
+namespace WordPuzzlesTest.Puzzle
+{
+    [TestFixture]
+    public class MultipleCluesPuzzleTest
+    {
+        [TestFixture]
+        public class GetCandidatesForLetter
+        {
+            [Test]
+            public void C_ReturnsExpectedCollection()
+            {
+                MultipleCluesPuzzle puzzle = new MultipleCluesPuzzle();
+                puzzle.RandomGeneratorSeed = 42;
+
+                List<string> candidates = puzzle.GetCandidatesForLetter('c');
+                Assert.Less(10, candidates.Count, "Expected at least 10 candidates.");
+                foreach (var candidate in candidates)
+                {
+                    //Console.WriteLine(candidate);
+                    Assert.AreEqual('c', candidate[1], "Expected second letter to be 'c'.");
+                }
+
+                candidates = puzzle.GetCandidatesForLetter('a');
+                Assert.Less(10, candidates.Count, "Expected at least 10 candidates.");
+                foreach (var candidate in candidates)
+                {
+                    Console.WriteLine(candidate);
+                    Assert.AreEqual('a', candidate[1], "Expected second letter to be 'a'.");
+                }
+
+                candidates = puzzle.GetCandidatesForLetter('t');
+                Assert.Less(10, candidates.Count, "Expected at least 10 candidates.");
+                foreach (var candidate in candidates)
+                {
+                    Console.WriteLine(candidate);
+                    Assert.AreEqual('t', candidate[1], "Expected second letter to be 't'.");
+                }
+            }
+        }
+
+        [TestFixture]
+        public class AddWordWithClues
+        {
+            [Test]
+            public void AddsWords()
+            {
+                MultipleCluesPuzzle puzzle = new MultipleCluesPuzzle();
+                puzzle.RandomGeneratorSeed = 42;
+
+                puzzle.AddWordWithClues("ace", new List<string>
+                {
+                    "Do well on a test",
+                    "Might be the high card?",
+                    "Fighter pilot"
+                });
+
+                Assert.AreEqual(4, puzzle.NextClueOrder, "Next clue should be 4.");
+                Assert.AreEqual("ace", puzzle.WordsWithClues[0].WordText);
+                Assert.AreEqual(6, puzzle.WordsWithClues[0].SumOfClueOrders);
+
+                puzzle.AddWordWithClues("hanger", new List<string>()
+                {
+                    "Word after cliff", 
+                    "Something often found in closets", 
+                    "Executioner?"
+                });
+
+                Assert.AreEqual(7, puzzle.NextClueOrder, "Next clue should be 7.");
+                Assert.AreEqual("hanger", puzzle.WordsWithClues[1].WordText);
+                Assert.AreEqual(15, puzzle.WordsWithClues[1].SumOfClueOrders);
+
+                puzzle.AddWordWithClues("atom", new List<string>()
+                {
+                    "Anagram of a watery castle defense",
+                    "Basic chemical element"
+                });
+
+                Assert.AreEqual(9, puzzle.NextClueOrder, "Next clue should be 9.");
+                Assert.AreEqual("atom", puzzle.WordsWithClues[2].WordText);
+                Assert.AreEqual(15, puzzle.WordsWithClues[2].SumOfClueOrders);
+            }
+        }
+
+        [TestFixture]
+        public class ReorderClues
+        {
+            [Test]
+            public void ChangesSums()
+            {
+                MultipleCluesPuzzle puzzle = new MultipleCluesPuzzle();
+                puzzle.RandomGeneratorSeed = 42;
+
+                puzzle.AddWordWithClues("ace", new List<string>
+                {
+                    "Do well on a test",
+                    "Might be the high card?",
+                    "Fighter pilot"
+                });
+
+                puzzle.AddWordWithClues("hanger", new List<string>()
+                {
+                    "Word after cliff",
+                    "Something often found in closets",
+                    "Executioner?"
+                });
+
+
+                puzzle.AddWordWithClues("atom", new List<string>()
+                {
+                    "Anagram of a watery castle defense",
+                    "Basic chemical element"
+                });
+                Assert.AreEqual(6, puzzle.WordsWithClues[0].SumOfClueOrders);
+                Assert.AreEqual(15, puzzle.WordsWithClues[1].SumOfClueOrders);
+                Assert.AreEqual(15, puzzle.WordsWithClues[2].SumOfClueOrders);
+
+                puzzle.ReorderClues();
+                int firstSum = puzzle.WordsWithClues[0].SumOfClueOrders;
+                int secondSum = puzzle.WordsWithClues[1].SumOfClueOrders;
+                int thirdSum = puzzle.WordsWithClues[2].SumOfClueOrders;
+
+                Assert.AreNotEqual(6, firstSum, "Expected first sum to be different");
+                Assert.AreNotEqual(15, secondSum, "Expected second sum to be different");
+                Assert.AreNotEqual(15, thirdSum, "Expected third sum to be different");
+
+                Assert.AreNotEqual(firstSum, secondSum, "Expected first != second");
+                Assert.AreNotEqual(firstSum, thirdSum, "Expected first != third");
+                Assert.AreNotEqual(thirdSum, secondSum, "Expected third != second");
+                Assert.AreEqual(36, firstSum + secondSum + thirdSum);
+
+
+            }
+        }
+    }
+}
