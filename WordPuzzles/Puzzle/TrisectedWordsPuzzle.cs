042bbda911510965ff4d39e34c7294029f800023
FlyingSheepGames <DungeonHero@FlyingSheep.com>
2021-03-11 14:43:54 -0500

    New Trisected Words Puzzle
    Initial version has everything except the format html functionality and IPuzzle implementation.

---
+++ WordPuzzles/Puzzle/TrisectedWordsPuzzle.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using WordPuzzles.Utility;
+
+namespace WordPuzzles.Puzzle
+{
+    public class TrisectedWordsPuzzle
+    {
+        WordRepository _repository = new WordRepository();
+        public string Solution { get; set; }
+
+        public List<TrisectedWord> FindWordsContainingLetters(char firstLetter, char secondLetter, char thirdLetter)
+        {
+
+            List<TrisectedWord> foundWords = new List<TrisectedWord>();
+
+            foreach (PatternAndTemplate patternTemplate in GetPatternTemplates())
+            {
+                string pattern = patternTemplate.Pattern;
+
+                pattern = pattern.Replace('1', firstLetter);
+                pattern = pattern.Replace('2', secondLetter);
+                pattern = pattern.Replace('3', thirdLetter);
+                if (Verbose)
+                {
+                    Console.WriteLine(pattern);
+                }
+                foreach (string foundWord in _repository.WordsMatchingPattern(pattern))
+                {
+                    foundWords.Add(new TrisectedWord()
+                    {
+                        Word =  foundWord, 
+                        Pattern = pattern,
+                        PatternAsEnumArray = patternTemplate.PatternAsEnumArray,
+                    });
+                }
+            }
+
+            return foundWords;
+        }
+
+        public bool Verbose { get; set; } = false;
+
+        private IEnumerable<PatternAndTemplate> GetPatternTemplates()
+        {
+            List<PatternAndTemplate> patternTemplates = new List<PatternAndTemplate>();
+            
+            StringBuilder patternBuilder = new StringBuilder();
+
+            foreach (SectionSubPattern[] sectionSubPattern in GetAllSectionSubPatterns())
+            {
+                patternBuilder.Clear();
+                for (int numberToSubstitue = 1; numberToSubstitue <= 3; numberToSubstitue++)
+                {
+                    switch (sectionSubPattern[numberToSubstitue-1])
+                    {
+                        case SectionSubPattern.FIRST_OF_TWO_LETTERS:
+                            patternBuilder.Append($"{numberToSubstitue}_");
+                            break;
+                        case SectionSubPattern.SECOND_OF_TWO_LETTERS:
+                            patternBuilder.Append($"_{numberToSubstitue}");
+                            break;
+                        case SectionSubPattern.FIRST_OF_THREE_LETTERS:
+                            patternBuilder.Append($"{numberToSubstitue}__");
+                            break;
+                        case SectionSubPattern.SECOND_OF_THREE_LETTERS:
+                            patternBuilder.Append($"_{numberToSubstitue}_");
+                            break;
+                        case SectionSubPattern.THIRD_OF_THREE_LETTERS:
+                            patternBuilder.Append($"__{numberToSubstitue}");
+                            break;
+                    }
+                }
+
+                patternTemplates.Add( new PatternAndTemplate()
+                {
+                    Pattern  = patternBuilder.ToString(),
+                    PatternAsEnumArray = sectionSubPattern,
+                });
+            }
+
+            return patternTemplates;
+        }
+
+        private IEnumerable<SectionSubPattern[]> GetAllSectionSubPatterns()
+        {
+            var allSectionSubPatterns = new List<SectionSubPattern[]>();
+            var sectionSubPattern = new SectionSubPattern[3];
+
+            foreach (SectionSubPattern firstSubPattern in Enum.GetValues(typeof(SectionSubPattern)))
+            foreach (SectionSubPattern secondSubPattern in Enum.GetValues(typeof(SectionSubPattern)))
+            foreach (SectionSubPattern thirdSubPattern in Enum.GetValues(typeof(SectionSubPattern)))
+            {
+                if (!IsAcceptableCombination(firstSubPattern, secondSubPattern, thirdSubPattern)) continue;
+                sectionSubPattern[0] = firstSubPattern;
+                sectionSubPattern[1] = secondSubPattern;
+                sectionSubPattern[2] = thirdSubPattern;
+                allSectionSubPatterns.Add(sectionSubPattern.Clone() as SectionSubPattern[]);
+            }
+
+            return allSectionSubPatterns;
+        }
+
+        private bool IsAcceptableCombination(SectionSubPattern firstSubPattern, SectionSubPattern secondSubPattern, SectionSubPattern thirdSubPattern)
+        {
+            if (firstSubPattern == SectionSubPattern.UNDEFINED) return false;
+            if (secondSubPattern == SectionSubPattern.UNDEFINED) return false;
+            if (thirdSubPattern == SectionSubPattern.UNDEFINED) return false;
+
+            int wordLength = CalculateLength(firstSubPattern) + CalculateLength(secondSubPattern) +
+                             CalculateLength(thirdSubPattern);
+            if (new List<int> {7, 8}.Contains(wordLength)) return true;
+            return false;
+        }
+
+        private int CalculateLength(SectionSubPattern firstSubPattern)
+        {
+            switch (firstSubPattern)
+            {
+                case SectionSubPattern.FIRST_OF_TWO_LETTERS:
+                case SectionSubPattern.SECOND_OF_TWO_LETTERS:
+                    return 2;
+                case SectionSubPattern.FIRST_OF_THREE_LETTERS:
+                case SectionSubPattern.SECOND_OF_THREE_LETTERS:
+                case SectionSubPattern.THIRD_OF_THREE_LETTERS:
+                    return 3;
+            }
+
+            throw new InvalidEnumArgumentException();
+        }
+
+        public List<TrisectedWord> GetNextWordCandidates()
+        {
+            char[] nextLetters = new char[3];
+            nextLetters[0] = '_';
+            nextLetters[1] = '_';
+            nextLetters[2] = '_';
+            int lettersFoundSoFar = 0;
+            int newCurrentIndexIfAllThreeAreUsed = 0;
+            int newCurrentIndexIfFirstTwoAreUsed = 0;
+            for (int currentIndex = CurrentIndex; currentIndex < Solution.Length; currentIndex++)
+            {
+                newCurrentIndexIfAllThreeAreUsed = currentIndex;
+                if (lettersFoundSoFar < 2)
+                {
+                    newCurrentIndexIfFirstTwoAreUsed = currentIndex;
+                }
+                char currentCharacter = Solution[currentIndex];
+                if (char.IsLetter(currentCharacter))
+                {
+                    nextLetters[lettersFoundSoFar++] = char.ToLowerInvariant(currentCharacter);
+                    if (3 <= lettersFoundSoFar) break;
+                }
+            }
+
+            if (lettersFoundSoFar == 0) return null;
+            //Best case - can use all three letters.
+            var findWordsContainingLetters = FindWordsContainingLetters(nextLetters[0], nextLetters[1], nextLetters[2]);
+            if (0 < findWordsContainingLetters.Count)
+            {
+                CurrentIndex = newCurrentIndexIfAllThreeAreUsed + 1;
+                return findWordsContainingLetters;
+            }
+            //Otherwise, let's try to use two of the letters. 
+            findWordsContainingLetters.AddRange(FindWordsContainingLetters(nextLetters[0], nextLetters[1], '_'));
+            findWordsContainingLetters.AddRange(FindWordsContainingLetters(nextLetters[0], '_', nextLetters[1]));
+            findWordsContainingLetters.AddRange(FindWordsContainingLetters('_', nextLetters[0], nextLetters[1]));
+            if (0 < findWordsContainingLetters.Count)
+            {
+                CurrentIndex = newCurrentIndexIfFirstTwoAreUsed + 1;
+                return findWordsContainingLetters;
+            }
+            throw new Exception($"Unable to and words that contain both {nextLetters[0]} and {nextLetters[1]}.");
+        }
+
+        public int CurrentIndex { get; set; } = 0;
+
+        public void AddClue(TrisectedWord wordWithClue)
+        {
+            if (string.IsNullOrWhiteSpace(wordWithClue.Clue))
+            {
+                wordWithClue.Clue = "Missing clue.";
+            }
+            Clues.Add(wordWithClue);
+        }
+
+        public IList<TrisectedWord> Clues { get; set; } = new List<TrisectedWord>();
+
+        public void CalculateWordSections()
+        {
+            foreach (var currentWord in Clues)
+            {
+                int currentIndexInWord = 0;
+                int wordLength = 0;
+                foreach (var patternEnum in currentWord.PatternAsEnumArray)
+                {
+                    switch (patternEnum)
+                    {
+                        case SectionSubPattern.FIRST_OF_TWO_LETTERS:
+                        case SectionSubPattern.SECOND_OF_TWO_LETTERS:
+                            wordLength = 2;
+                            break;
+                        case SectionSubPattern.FIRST_OF_THREE_LETTERS:
+                        case SectionSubPattern.SECOND_OF_THREE_LETTERS:
+                        case SectionSubPattern.THIRD_OF_THREE_LETTERS:
+                            wordLength = 3;
+                            break;
+                    }
+
+                    var currentWordSection = currentWord.Word.Substring(currentIndexInWord, wordLength);
+                    currentIndexInWord += wordLength;
+                    WordSections.Add(currentWordSection);
+                }
+            }
+        }
+
+        public List<string> WordSections { get; set; } = new List<string>();
+    }
+
+    public class PatternAndTemplate 
+    {
+        public string Pattern { get; set; }
+        public SectionSubPattern[] PatternAsEnumArray { get; set; }
+    }
+
+    public class TrisectedWord
+    {
+        public string Word { get; set; }
+        public string Pattern { get; set; }
+        public SectionSubPattern[] PatternAsEnumArray { get; set; }
+        public string Clue { get; set; }
+    }
+
+    public enum SectionSubPattern
+    {
+        UNDEFINED,
+        FIRST_OF_TWO_LETTERS,
+        FIRST_OF_THREE_LETTERS,
+        SECOND_OF_TWO_LETTERS,
+        SECOND_OF_THREE_LETTERS,
+        THIRD_OF_THREE_LETTERS
+    }
+}
---
+++ WordPuzzlesTest/Puzzle/TrisectedWordsTest.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WordPuzzles.Puzzle;
+using WordPuzzles.Utility;
+
+namespace WordPuzzlesTest.Puzzle
+{
+    [TestFixture]
+    public class TrisectedWordsTest
+    {
+        [TestFixture]
+        public class UnrelatedTests
+        {
+            [Test]
+            public void AreThereEnoughSevenAndEightLetterWords()
+            {
+                WordRepository wordRepository = new WordRepository() {};
+
+                int countOfSevenLetterWords = wordRepository.WordsMatchingPattern("_______").Count;
+                int countOfEightLetterWords = wordRepository.WordsMatchingPattern("________").Count;
+                Console.WriteLine($"There are {countOfSevenLetterWords} seven-letter words and {countOfEightLetterWords} eight-letter words");
+                Assert.LessOrEqual(80, countOfSevenLetterWords, "Expected at least 80 seven-letter words.");
+                Assert.LessOrEqual(80, countOfEightLetterWords, "Expected at least 80 eight-letter words.");
+            }
+        }
+
+        [TestFixture]
+        public class FindWordsContainingLetters
+        {
+            [Test]
+            public void THE_FindsAtLeastOneWord()
+            {
+                TrisectedWordsPuzzle trisectedWordsPuzzle = new TrisectedWordsPuzzle();
+                var foundWords = trisectedWordsPuzzle.FindWordsContainingLetters('t', 'h', 'e');
+                foreach (var word in foundWords)
+                {
+                    Console.WriteLine(word.Word);
+                }
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var firstWord = foundWords[0];
+                Assert.AreEqual("torched", firstWord.Word, "Expected first word to be torched ");
+                Assert.AreEqual("t___he_", firstWord.Pattern, "Unexpected pattern.");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[0], "Unexpected PatternAsEnumArray[0]");
+                Assert.AreEqual(SectionSubPattern.THIRD_OF_THREE_LETTERS, firstWord.PatternAsEnumArray[1], "Unexpected PatternAsEnumArray[1]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[2], "Unexpected PatternAsEnumArray[2]");
+            }
+        }
+
+        [TestFixture]
+        public class GetNextWordCandidates
+        {
+            [Test]
+            public void THE_ReturnsExpectedResults()
+            {
+                TrisectedWordsPuzzle trisectedWordsPuzzle = new TrisectedWordsPuzzle();
+                trisectedWordsPuzzle.Solution = "The";
+                var foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                foreach (var word in foundWords)
+                {
+                    Console.WriteLine(word.Word);
+                }
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var firstWord = foundWords[0];
+                Assert.AreEqual("torched", firstWord.Word, "Expected first word to be torched ");
+                Assert.AreEqual("t___he_", firstWord.Pattern, "Unexpected pattern.");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[0], "Unexpected PatternAsEnumArray[0]");
+                Assert.AreEqual(SectionSubPattern.THIRD_OF_THREE_LETTERS, firstWord.PatternAsEnumArray[1], "Unexpected PatternAsEnumArray[1]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[2], "Unexpected PatternAsEnumArray[2]");
+
+                Assert.IsNull(trisectedWordsPuzzle.GetNextWordCandidates(), "We should be at the end of the solution now.");
+            }
+
+            [Test]
+            public void THE_IgnoresSpacesAndSymbols()
+            {
+                TrisectedWordsPuzzle trisectedWordsPuzzle = new TrisectedWordsPuzzle();
+                trisectedWordsPuzzle.Solution = "T  h . $#^&*e %&*($#";
+                var foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                foreach (var word in foundWords)
+                {
+                    Console.WriteLine(word.Word);
+                }
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var firstWord = foundWords[0];
+                Assert.AreEqual("torched", firstWord.Word, "Expected first word to be torched ");
+                Assert.AreEqual("t___he_", firstWord.Pattern, "Unexpected pattern.");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[0], "Unexpected PatternAsEnumArray[0]");
+                Assert.AreEqual(SectionSubPattern.THIRD_OF_THREE_LETTERS, firstWord.PatternAsEnumArray[1], "Unexpected PatternAsEnumArray[1]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[2], "Unexpected PatternAsEnumArray[2]");
+
+                Assert.IsNull(trisectedWordsPuzzle.GetNextWordCandidates(), "We should be at the end of the solution now.");
+            }
+
+            [Test]
+            public void TWO_WORDS_ReturnsExpectedResults()
+            {
+                TrisectedWordsPuzzle trisectedWordsPuzzle = new TrisectedWordsPuzzle() {Verbose = true};
+                trisectedWordsPuzzle.Solution = "Two words.";
+                var foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                foreach (var word in foundWords)
+                {
+                    Console.WriteLine($"First word { word.Word}");
+                }
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var firstWord = foundWords[0];
+                Assert.AreEqual("tawnier", firstWord.Word, "Expected first word to be tawnier ");
+                Assert.AreEqual("t_w____", firstWord.Pattern, "Unexpected pattern.");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[0], "Unexpected PatternAsEnumArray[0]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, firstWord.PatternAsEnumArray[1], "Unexpected PatternAsEnumArray[1]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_THREE_LETTERS, firstWord.PatternAsEnumArray[2], "Unexpected PatternAsEnumArray[2]");
+
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                foreach (var word in foundWords)
+                {
+                    Console.WriteLine($"Second word { word.Word}");
+                }
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var secondWord = foundWords[0];
+                Assert.AreEqual("outwear", secondWord.Word, "Expected second word to be tawnier ");
+                Assert.AreEqual("o__w___", secondWord.Pattern, "Unexpected pattern.");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS,   secondWord.PatternAsEnumArray[0], "Unexpected PatternAsEnumArray[0]");
+                Assert.AreEqual(SectionSubPattern.SECOND_OF_TWO_LETTERS,   secondWord.PatternAsEnumArray[1], "Unexpected PatternAsEnumArray[1]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_THREE_LETTERS, secondWord.PatternAsEnumArray[2], "Unexpected PatternAsEnumArray[2]");
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                foreach (var word in foundWords)
+                {
+                    Console.WriteLine($"Third word { word.Word}");
+                }
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var thirdWord = foundWords[0];
+                Assert.AreEqual("overbid", thirdWord.Word, "Expected third word to be overbid ");
+                Assert.AreEqual("o__r__d", thirdWord.Pattern, "Unexpected pattern.");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, thirdWord.PatternAsEnumArray[0], "Unexpected PatternAsEnumArray[0]");
+                Assert.AreEqual(SectionSubPattern.SECOND_OF_TWO_LETTERS, thirdWord.PatternAsEnumArray[1], "Unexpected PatternAsEnumArray[1]");
+                Assert.AreEqual(SectionSubPattern.THIRD_OF_THREE_LETTERS, thirdWord.PatternAsEnumArray[2], "Unexpected PatternAsEnumArray[2]");
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                foreach (var word in foundWords)
+                {
+                    Console.WriteLine($"Fourth word { word.Word}");
+                }
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var fourthWord = foundWords[0];
+                Assert.AreEqual("salient", fourthWord.Word, "Expected third word to be salient ");
+                Assert.AreEqual("s______", fourthWord.Pattern, "Unexpected pattern.");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, fourthWord.PatternAsEnumArray[0], "Unexpected PatternAsEnumArray[0]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_TWO_LETTERS, fourthWord.PatternAsEnumArray[1], "Unexpected PatternAsEnumArray[1]");
+                Assert.AreEqual(SectionSubPattern.FIRST_OF_THREE_LETTERS, fourthWord.PatternAsEnumArray[2], "Unexpected PatternAsEnumArray[2]");
+
+                Assert.IsNull(trisectedWordsPuzzle.GetNextWordCandidates(), "We should be at the end of the solution now.");
+            }
+        }
+
+        [TestFixture]
+        public class AddClue
+        {
+            [Test]
+            public void TWO_WORDS_ReturnsExpectedResults()
+            {
+                TrisectedWordsPuzzle trisectedWordsPuzzle = new TrisectedWordsPuzzle() { Verbose = true };
+                trisectedWordsPuzzle.Solution = "Two words.";
+                var foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var firstWord = foundWords[0];
+                Assert.AreEqual("tawnier", firstWord.Word, "Expected first word to be tawnier ");
+                firstWord.Clue = "More tawny.";
+                trisectedWordsPuzzle.AddClue(firstWord);
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var secondWord = foundWords[0];
+                Assert.AreEqual("outwear", secondWord.Word, "Expected second word to be tawnier ");
+                secondWord.Clue = "Use clothing until it's no longer needed.";
+                trisectedWordsPuzzle.AddClue(secondWord);
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var thirdWord = foundWords[0];
+                Assert.AreEqual("overbid", thirdWord.Word, "Expected third word to be overbid ");
+                thirdWord.Clue = "Pay too much on EBay";
+                trisectedWordsPuzzle.AddClue(thirdWord);
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                Assert.LessOrEqual(1, foundWords.Count, "Found at least one word.");
+                var fourthWord = foundWords[0];
+                Assert.AreEqual("salient", fourthWord.Word, "Expected third word to be salient ");
+                fourthWord.Clue = "Relevant";
+                trisectedWordsPuzzle.AddClue(fourthWord);
+
+                Assert.IsNull(trisectedWordsPuzzle.GetNextWordCandidates(), "We should be at the end of the solution now.");
+
+                Assert.AreEqual(4, trisectedWordsPuzzle.Clues.Count, "Expected 4 clues.");
+            }
+        }
+
+        [TestFixture]
+        public class CalculateWordSections
+        {
+
+            [Test]
+            public void TWO_WORDS_CreatesExpectedProperty()
+            {
+                //arrange
+                TrisectedWordsPuzzle trisectedWordsPuzzle = new TrisectedWordsPuzzle() {  };
+                trisectedWordsPuzzle.Solution = "Two words.";
+
+                var foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                var firstWord = foundWords[0];
+                firstWord.Clue = "More tawny.";
+                trisectedWordsPuzzle.AddClue(firstWord);
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                var secondWord = foundWords[0];
+                secondWord.Clue = "Use clothing until it's no longer needed.";
+                trisectedWordsPuzzle.AddClue(secondWord);
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                var thirdWord = foundWords[0];
+                thirdWord.Clue = "Pay too much on EBay";
+                trisectedWordsPuzzle.AddClue(thirdWord);
+
+                foundWords = trisectedWordsPuzzle.GetNextWordCandidates();
+                var fourthWord = foundWords[0];
+                fourthWord.Clue = "Relevant";
+                trisectedWordsPuzzle.AddClue(fourthWord);
+
+                //act
+                trisectedWordsPuzzle.CalculateWordSections();
+
+                //assert
+                Assert.IsNull(trisectedWordsPuzzle.GetNextWordCandidates(), "We should be at the end of the solution now.");
+
+                Assert.AreEqual(4, trisectedWordsPuzzle.Clues.Count, "Expected 4 clues.");
+                foreach (var section in trisectedWordsPuzzle.WordSections)
+                {
+                    Console.WriteLine(section);
+                }
+                Assert.AreEqual(12, trisectedWordsPuzzle.WordSections.Count, "Expected 12 word sections.");
+                CollectionAssert.AreEquivalent(new List<string>
+                    { 
+                        "ta", "wn", "ier", 
+                        "ou", "tw", "ear", 
+                        "ov", "er", "bid", 
+                        "sa", "li", "ent"
+                    }, trisectedWordsPuzzle.WordSections);
+            }
+        }
+    }
+}
